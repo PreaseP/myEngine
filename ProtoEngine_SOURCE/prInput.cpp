1963a95d6b84fad0f7c5e3e268b1aacd0f8150f4
PreaseP@H <dong123541@gmail.com>
2026-02-03 02:54:44 +0900

    Input 클래스 함수 리팩토링

--- ProtoEngine_SOURCE/prInput.cpp
+++ ProtoEngine_SOURCE/prInput.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "prInput.h"
 
 
@@ -10,12 +12,21 @@ namespace pr
 		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 		'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
 		'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+		VK_LEFT, VK_RIGHT, VK_DOWN, VK_UP,
 	};
 
 	void Input::Initialize()
 	{
-		mKeys.resize((UINT)eKeyCode::End);
+		createKeys();
+	}
 
+	void Input::Update()
+	{
+		updateKeys();
+	}
+
+	void Input::createKeys()
+	{
 		for (size_t i = 0; i < (UINT)eKeyCode::End; ++i)
 		{
 			Key key = {};
@@ -27,29 +38,49 @@ namespace pr
 		}
 	}
 
-	void Input::Update()
+	void Input::updateKeys()
 	{
-		for (size_t i = 0; i < mKeys.size(); ++i)
+		std::for_each(mKeys.begin(), mKeys.end(),
+			[](Key& key) -> void
+			{
+				updateKey(key);
+			});
+	}
+
+	void Input::updateKey(Input::Key& key)
+	{
+		if (isKeyDown(key.KeyCode))
 		{
-			// 키가 눌렸다
-			if (GetAsyncKeyState(ASCII[i]) & 0x8000)
-			{
-				if (mKeys[i].bPressed == true)
-					mKeys[i].state = eKeyState::Pressed;
-				else
-					mKeys[i].state = eKeyState::Down;
-
-				mKeys[i].bPressed = true;
-			}
-			else // 키가 눌리지 않았다
-			{
-				if (mKeys[i].bPressed == true)
-					mKeys[i].state = eKeyState::Up;
-				else
-					mKeys[i].state = eKeyState::None;
-
-				mKeys[i].bPressed = false;
-			}
+			updateKeyDown(key);
 		}
+		else
+		{
+			updateKeyUp(key);
+		}
+	}
+
+	bool Input::isKeyDown(eKeyCode code)
+	{
+		return GetAsyncKeyState(ASCII[(UINT)code]) & 0x8000;
+	}
+
+	void Input::updateKeyDown(Input::Key& key)
+	{
+		if (key.bPressed == true)
+			key.state = eKeyState::Pressed;
+		else
+			key.state = eKeyState::Down;
+
+		key.bPressed = true;
+	}
+
+	void Input::updateKeyUp(Input::Key& key)
+	{
+		if (key.bPressed == true)
+			key.state = eKeyState::Up;
+		else
+			key.state = eKeyState::None;
+
+		key.bPressed = false;
 	}
 }
--- ProtoEngine_SOURCE/prInput.h
+++ ProtoEngine_SOURCE/prInput.h
@@ -16,6 +16,7 @@ namespace pr
 		Q, W, E, R, T, Y, U, I, O, P,
 		A, S, D, F, G, H, J, K, L,
 		Z, X, C, V, B, N, M,
+		Left, RIght, Down, Up,
 		End,
 	};
 
@@ -31,13 +32,18 @@ namespace pr
 		
 		static void Initialize();
 		static void Update();
-
+		
 		static bool GetKeyDown(eKeyCode code) { return mKeys[(UINT)code].state == eKeyState::Down; };
 		static bool GetKeyUp(eKeyCode code) { return mKeys[(UINT)code].state == eKeyState::Up; };
 		static bool GetKey(eKeyCode code) { return mKeys[(UINT)code].state == eKeyState::Pressed; };
 
 	private:
-
+		static void createKeys();
+		static void updateKeys();
+		static void updateKey(Input::Key& key);
+		static bool isKeyDown(eKeyCode code);
+		static void updateKeyDown(Input::Key& key);
+		static void updateKeyUp(Input::Key& key);
 
 	private:
 		//eKeyState mState[] = eKeyState::Up;
